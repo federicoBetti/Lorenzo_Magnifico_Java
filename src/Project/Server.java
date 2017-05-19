45c6cffe047748d40c63f56bbb2a9b72b0069750
Raffaele Bongo <raffaele.bongo@mail.polimi.it>
2017-05-19 16:36:14 +0200

    cambiamenti storici

--- src/Project/Server.java
+++ src/Project/Server.java
@@ -1,10 +1,12 @@
 package Project;
 import Project.Configurations.*;
+import com.google.gson.Gson;
 
 /**
  * Created by raffaelebongo on 18/05/17.
  */
 public class Server {
+
 
     Configuration configuration = new Configuration();
     public Server(){
